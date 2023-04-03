9d56121d25d821227cdbfe6ceecf9ddae0ad09d9
Gonzalo Sola <gsola@culturait.com>
2023-04-02 22:36:02 -0300

    Ajustes

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Loading from './components/Loading';
-import { StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, View } from "react-native";
+import { useEffect } from 'react';
+import backgroundTaTeTi from "./assets/ttt_board.png";
 
 export default function App() {
+  
+  const [load, setLoad] = useState(true);
+
+  useEffect(() => {
+    setInterval(() => {
+      setLoad((load)=>!load);
+    }, 6000);
+  }, [load]);
   return (
     <View style={styles.container}>
-      <Loading/>
+      {load ? null : <Loading></Loading>}
+      {load ? null: <Image
+        source={backgroundTaTeTi}
+        resizeMode="stretch"
+        style={styles.image}
+      /> }
     </View>
   );
 }
@@ -13,12 +28,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'lightblue',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  loading: {
-    fontSize: 60,
-    alignItems: 'center'
-  }
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#0000000",
+  },
+  image: {
+    width: "85%",
+    height: "60%",
+    marginLeft: 10,
+  },
 });
